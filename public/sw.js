2a4c856c02f6af0e39316525851566b46088f0a3
yoshikouki <yoshikouki@gmail.com>
2023-11-11 23:15:28 +0900

    Force activation of sw.js changes

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,17 @@
 self.addEventListener("install", (event) => {
   console.log("Installing Service Worker...", new Date().toLocaleTimeString());
   event.waitUntil(() => {
+    self.skipWaiting();
     console.log("Installed Service Worker.", new Date().toLocaleTimeString());
   });
 });
 
 self.addEventListener("activate", (event) => {
   console.log("Activated Service Worker.", new Date().toLocaleTimeString());
+  event.waitUntil(() => {
+    clients.claim();
+    console.log("Activated Service Worker.", new Date().toLocaleTimeString());
+  });
 });
 
 self.addEventListener("fetch", (event) => {
