a823e0a978bc17a43ae9a0c2b49cd1344e333d1d
yoshikouki <yoshikouki@gmail.com>
2023-11-12 11:35:08 +0900

    Enable PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,49 @@ import { Footer } from "./Footer";
 import "./globals.css";
 import { font } from "./theme";
 
+const APP_NAME = "Pmdr";
+const APP_DEFAULT_TITLE = "Pmdr";
+const APP_TITLE_TEMPLATE = "%s - Pmdr";
+const APP_DESCRIPTION = "Fantastic Pomodoro Timer";
+
 export const metadata: Metadata = {
-  title: "Pmdr",
-  description: "Fantastic Pomodoro Timer",
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#000000",
 };
 
 export default function RootLayout({
