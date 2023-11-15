88bd121272f663225cfe72bc883c91480cc764cd
yoshikouki <yoshikouki@gmail.com>
2023-11-15 23:04:54 +0900

    No longer needed since migrated to workbox

--- src/components/Timer/PmdrProvider.tsx
+++ src/components/Timer/PmdrProvider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { initializePmdr } from "@pmdr/core";
-import { ReactNode, createContext, useContext, useEffect } from "react";
+import { ReactNode, createContext, useContext } from "react";
 
 const PmdrContext = createContext({
   pmdr: initializePmdr(),
@@ -9,19 +9,6 @@ const PmdrContext = createContext({
 
 export const PmdrProvider = ({ children }: { children: ReactNode }) => {
   const pmdr = initializePmdr();
-
-  useEffect(() => {
-    if (!navigator.serviceWorker) return;
-
-    navigator.serviceWorker.register("/sw.js").then((registration) => {
-      registration.addEventListener("message", (event) => {
-        console.log("message on registered", event.data);
-      });
-    });
-    navigator.serviceWorker.addEventListener("message", (event) => {
-      console.log("message", event.data.action);
-    });
-  }, []);
 
   return (
     <PmdrContext.Provider value={{ pmdr }}>{children}</PmdrContext.Provider>
