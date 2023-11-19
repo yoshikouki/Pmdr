d643c027e6c29d638c22ed6f3d14e9a3f1f5718d
yoshikouki <yoshikouki@gmail.com>
2023-11-19 16:37:02 +0900

    Update list of notification settings

--- src/app/settings/notification-settings.tsx
+++ src/app/settings/notification-settings.tsx
@@ -1,26 +1,76 @@
 "use client";
 
+import { BellRing, Check, GalleryVerticalEnd, Vibrate } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Switch } from "@/components/ui/switch";
+import { Paragraph } from "@/components/ui/typography";
+import { cn } from "@/lib/utils";
 
-export const NotificationSettings: React.FC = () => {
+type CardProps = React.ComponentProps<typeof Card>;
+
+export const NotificationSettings: React.FC = ({
+  className,
+  ...props
+}: CardProps) => {
   return (
-    <Card>
+    <Card className={cn(className)} {...props}>
       <CardHeader>
         <CardTitle>Notifications</CardTitle>
-        <CardDescription>Card Description</CardDescription>
       </CardHeader>
-      <CardContent>
-        <p>Currently not logged in.</p>
+
+      <CardContent className="grid gap-4">
+        <div className="flex items-center space-x-4">
+          <BellRing />
+          <div className="flex-1 space-y-1">
+            <Paragraph className="leading-none">All Notifications</Paragraph>
+            <Paragraph variant="muted" size="sm">
+              Turn on or off notifications for the entire app.
+            </Paragraph>
+          </div>
+          <Switch />
+        </div>
       </CardContent>
+
+      <CardContent className="grid gap-4">
+        <div className="flex items-center space-x-4">
+          <Vibrate />
+          <div className="flex-1 space-y-1">
+            <Paragraph className="leading-none">
+              Web Push Notifications
+            </Paragraph>
+            <Paragraph variant="muted" size="sm">
+              Get notifications through your web browser.
+            </Paragraph>
+          </div>
+          <Switch />
+        </div>
+      </CardContent>
+
+      <CardContent className="grid gap-4">
+        <div className="flex items-center space-x-4">
+          <GalleryVerticalEnd />
+          <div className="flex-1 space-y-1">
+            <Paragraph className="leading-none">In-App Notifications</Paragraph>
+            <Paragraph variant="muted" size="sm">
+              Receive notifications while using the app.
+            </Paragraph>
+          </div>
+          <Switch />
+        </div>
+      </CardContent>
+
       <CardFooter>
-        <p>Card Footer</p>
+        <Button variant="outline" className="w-full">
+          <Check className="mr-2 h-4 w-4" /> Test
+        </Button>
       </CardFooter>
     </Card>
   );
